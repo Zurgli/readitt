c01e84a501008447126463b095530f685f184ebd
Raghu Betina <raghu@firstdraft.com>
2017-08-17 03:45:21 +0000

    Whitelist columns in PVote for ActiveAdmin

--- app/admin/p_vote.rb
+++ app/admin/p_vote.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PVote do
 
+ permit_params :up, :user_id, :post_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
