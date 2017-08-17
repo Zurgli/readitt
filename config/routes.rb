93cc854c9ac93d114de0dc84128959e9752d9ca6
Raghu Betina <raghu@firstdraft.com>
2017-08-17 03:44:55 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "topics#index"
   # Routes for the C_vote resource:
   # CREATE
