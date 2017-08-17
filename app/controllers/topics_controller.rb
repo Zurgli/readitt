4dfcd72c65c7e6b819fc85d56d7930469d34cef9
Raghu Betina <raghu@firstdraft.com>
2017-08-17 03:43:35 +0000

    Generate topics resource with starter:resource

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,81 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+
+    render("topics/index.html.erb")
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+
+    render("topics/show.html.erb")
+  end
+
+  def new
+    @topic = Topic.new
+
+    render("topics/new.html.erb")
+  end
+
+  def create
+    @topic = Topic.new
+
+    @topic.name = params[:name]
+    @topic.subject = params[:subject]
+
+    save_status = @topic.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/topics/new", "/create_topic"
+        redirect_to("/topics")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Topic created successfully.")
+      end
+    else
+      render("topics/new.html.erb")
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+
+    render("topics/edit.html.erb")
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    @topic.name = params[:name]
+    @topic.subject = params[:subject]
+
+    save_status = @topic.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/topics/#{@topic.id}/edit", "/update_topic"
+        redirect_to("/topics/#{@topic.id}", :notice => "Topic updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Topic updated successfully.")
+      end
+    else
+      render("topics/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    @topic.destroy
+
+    if URI(request.referer).path == "/topics/#{@topic.id}"
+      redirect_to("/", :notice => "Topic deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Topic deleted.")
+    end
+  end
+end
---
+++ db/migrate/20170817034335_create_topics.rb
@@ -0,0 +1,11 @@
+class CreateTopics < ActiveRecord::Migration
+  def change
+    create_table :topics do |t|
+      t.string :name
+      t.string :subject
+
+      t.timestamps
+
+    end
+  end
+end
