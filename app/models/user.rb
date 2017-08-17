9951aa74b1fd6a775bd3297507d3ef64e40fba80
Raghu Betina <raghu@firstdraft.com>
2017-08-17 03:44:02 +0000

    Direct association between users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :posts,
+             :dependent => :nullify
+
   has_many   :subscriptions,
              :dependent => :destroy
 
