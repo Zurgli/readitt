38376e79135f7d7c91698d37ea0cac11b5f02789
Raghu Betina <raghu@firstdraft.com>
2017-08-17 03:45:26 +0000

    Whitelist columns in CVote for ActiveAdmin

--- app/admin/c_vote.rb
+++ app/admin/c_vote.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CVote do
 
+ permit_params :up, :user_id, :comment_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
